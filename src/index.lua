e5eed1eda00f0b08e67945bf4f3d3f0303abd22a
cfgtsp <69192198+cfgtsp5192@users.noreply.github.com>
2022-05-22 20:16:47 +0800

    Create index.lua

---
+++ src/index.lua
@@ -0,0 +1,20 @@
+--[[
+
+Index metamethod hook
+
+]]
+
+local IIIIlIlIIlII_zrismj = _G["\103\97\109\101"];
+local llIIllIlIlIl_tfhwnl = _G["\104\111\111\107\109\101\116\97\109\101\116\104\111\100"];
+local IlIlIllIIllI_rwkwnd = _G["\110\101\119\99\99\108\111\115\117\114\101"];
+
+local IlIIIIIllIlI_wfvgdy = function(...)
+	local IllIIIlllIIl_smoyle = {...};
+	
+	local llIIlIIlIlll_lekngl = IllIIIlllIIl_smoyle[1];
+	local llIlllIIIllI_xpgsxx = IllIIIlllIIl_smoyle[2];
+	local lllllllllIII_ymrpxv = IllIIIlllIIl_smoyle[3];
+end
+
+local IlIlIllIlIII_dizjka = IlIlIllIIllI_rwkwnd(IlIIIIIllIlI_wfvgdy);
+local IlIIlllIlIlI_vonsoh = llIIllIlIlIl_tfhwnl(IIIIlIlIIlII_zrismj, "\105\110\100\101\120", IlIlIllIlIII_dizjka);
